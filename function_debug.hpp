358c39c78080b61da30158e84eb5925ba5f4ba20
ecb <ecb@hostname.hep.manchester.ac.uk>
2018-03-09 12:23:47 +0000

    fixed compile errors

--- function_debug.hpp
+++ function_debug.hpp
@@ -1,6 +1,7 @@
 
 #include <map>
 #include <string>
+#include <cstdarg>
 
 
 #if DEBUG_MESSAGE_DISABLE
@@ -34,10 +35,32 @@ bool is_last(Iter iter, const Cont& cont)
 }
 */
 
-std::string operator<<(std::string& l, const std::string& r)
+/*
+   std::string operator<<(std::string& l, const char r[])
 {
-    return l += r;
+    return l + std::string(r);
 }
+*/
+
+std::string operator<<(std::string& l, const char* const r)
+{
+    return l + std::string(r);
+}
+
+std::string operator<<(const std::string& l, const std::string& r)
+{
+    return l + r;
+}
+
+/*
+template <typename T>
+std::string argument_convert(const T& t)
+{
+    std::ostringstream os;
+    os << t;
+    return os;
+}
+*/
 
 // create string containing function debug information including arguments
 // information
@@ -48,16 +71,43 @@ std::string function_debug_arguments(const char* function_string, std::map<const
 {
     std::string ret(function_string);
     //ret << "(");
-    ret << "(";
+    ret << std::string("(");
     std::map<const std::string, const std::string>::const_iterator it{arguments.begin()};
     for(; it != arguments.end(); ++ it)
     {
-        ret << it->first << "=" << it->second;
-        if(it != --arguments.end()) ret << ", ";
+        ret << it->first << std::string("=") << it->second;
+        if(it != --arguments.end()) ret << std::string(", ");
         //if(!is_last(it, arguments)) ret + std::string(",");
     }
     ret << ")";
     return ret;
+}
+
+// all arguments have to be of type string
+// which isn't a significant improvement over the map method
+// however it is a slightly more convenient syntax
+// instead of organizing arguments into pairs of name and values
+// the function takes a continual list of strings, and these should
+// be formatted into names and values format
+// count is the number of arguments, however twice that number of
+// strings must be provided
+std::string function_debug_arguments(const char* function_string, const int count, ...)
+{
+    va_list vargs;
+    va_start(vargs, count);
+
+    std::ostringstream os;
+    os << function_string;
+    os << "(";
+    for(int i{0}; i < count; ++ i)
+    {
+        os << va_arg(vargs, std::string) << "=" << va_arg(vargs, std::string);
+        if(i < count - 1) os << ", ";
+    }
+    os << ")";
+    
+    va_end(vargs);
+    return os.str();
 }
 
 // create string containing function debug information including local
@@ -69,8 +119,8 @@ std::string function_debug_locals(const char* function_string, std::map<const st
     std::map<const std::string, const std::string>::const_iterator it{locals.begin()};
     for(; it != locals.end(); ++ it)
     {
-        ret << it->first << "=" << it->second;
-        if(it != --locals.end()) ret << ", ";
+        ret << it->first << std::string("=") << it->second;
+        if(it != --locals.end()) ret << std::string(", ");
         //if(!is_last(it, locals)) ret + std::string(",");
     }
     return ret;
